97076d64dcdec3501b29dd26edddd2e0dbdc4695
Jose Rodriguez <jolurodr@bancolombia.com.co>
2021-11-13 15:41:18 -0500

    firs commit solidity

---
+++ contracts/HelloBlockchain.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloBlockChain{
+
+  function sayHi() public pure returns(string memory){
+    return "!Hello Blockchain";
+  }
+}
